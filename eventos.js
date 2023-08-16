c1cf7c80892cf70ba8c22d3e0a04bdae0bff813b
facundotravers <letrasft@gmail.com>
2023-08-16 15:18:51 -0300

    Soy el Div

---
+++ eventos.js
@@ -0,0 +1,8 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+    const divContenedor = document.querySelector('.boton-container');
+
+    divContenedor.addEventListener('click', function () {
+        alert('Hola! Soy el div');
+    });
+});
