ce8c65c3d8729973d841a06c4879cad2e67dd763
facundotravers <letrasft@gmail.com>
2023-08-16 15:34:35 -0300

    Solución Click

--- eventos.js
+++ eventos.js
@@ -1,8 +1,15 @@
+const boton = document.querySelector('button');
+const divContenedor = document.querySelector('.boton-container');
 
-document.addEventListener('DOMContentLoaded', function () {
-    const divContenedor = document.querySelector('.boton-container');
+function mostrarSaludo() {
+    alert('¡Hola desde el botón!');
+}
 
-    divContenedor.addEventListener('click', function () {
-        alert('Hola! Soy el div');
-    });
+boton.addEventListener('click', function (event) {
+    event.stopPropagation();
+    mostrarSaludo();
+});
+
+divContenedor.addEventListener('click', function () {
+    alert('Hola! Soy el div');
 });
