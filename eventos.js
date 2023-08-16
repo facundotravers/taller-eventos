04334fa4bfc2470acecf528a69e8b47c4dfa491c
facundotravers <letrasft@gmail.com>
2023-08-16 15:37:16 -0300

    Update eventos.js

--- eventos.js
+++ eventos.js
@@ -1,15 +1,7 @@
-const boton = document.querySelector('button');
-const divContenedor = document.querySelector('.boton-container');
+document.addEventListener('DOMContentLoaded', function () {
+    const divContenedor = document.querySelector('.boton-container');
 
-function mostrarSaludo() {
-    alert('¡Hola desde el botón!');
-}
-
-boton.addEventListener('click', function (event) {
-    event.stopPropagation();
-    mostrarSaludo();
-});
-
-divContenedor.addEventListener('click', function () {
-    alert('Hola! Soy el div');
+    divContenedor.addEventListener('click', function () {
+        alert('Hola! Soy el div');
+    });
 });
